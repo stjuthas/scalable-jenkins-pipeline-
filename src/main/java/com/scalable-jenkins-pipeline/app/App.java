d0c777fa1ba6c612d42698921ee1ebdaa07df12d
juthas okari <stjuthas@yahoo.com>
2021-12-27 15:02:07 -0600

    Sample maven project

---
+++ src/main/java/com/scalable-jenkins-pipeline/app/App.java
@@ -0,0 +1,13 @@
+package com.scalable-jenkins-pipeline.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
